c6dec20575abda1be6a799ade4a6910b31e32871
mousyhead <franc.lorusso@gmail.com>
2014-04-10 19:33:03 +0200

    ...

--- src/it/skedenpio/Concorrente.java
+++
@@ -1,5 +0,0 @@
-package it.skedenpio;
-
-public class Concorrente {
-
-}
--- src/it/skedenpio/Turn.java
+++ src/it/skedenpio/Turn.java
@@ -6,25 +6,55 @@ public class Turn {
 	
 	public void execute() {
 		
+		clearConsole();
 		Deck deck = new Deck(); //Shuffle
 		
+		// TURNO PLAYER1
 		Player player1 = new Player();
-		player1.setScore(0);
-		
-		while ( moreCards() )
-		{
-			Card card = deck.getCard(); //dai carta
-			System.out.println(card.getNumero()+" di "+card.getSeme()+". Punti: "+calcValue(card));
+		player1.setScore(0);		
+		do {
+			Card card = deck.getCard(); //dai carta	
 			player1.setScore( player1.getScore() + calcValue(card) );
-			System.out.println( "TOTALE: " + player1.getScore() );
-		} 
+			if ( isSballato(player1) ) {
+				System.out.println( "TOTALE: " + player1.getScore() );
+				System.out.println( "HAI SBALLATO!!!" );				
+				Replay();
+			} else {
+				deck.getCard(); //dai carta
+				System.out.println(card.getNumero()+" di "+card.getSeme()+". Punti: "+calcValue(card));
+				System.out.println( "TOTALE: " + player1.getScore() );
+				System.out.println("CARTA (C) | STO BENE (S)");
+			}
+		} while ( moreCards() && !isSballato(player1) );
+		
+		
+		
+
+		
+		// TURNO DEALER
+		if ( !isSballato(player1) ) {
+			Player dealer = new Player();
+			dealer.setScore(0);
+			
+			System.out.println("TURNO DEALER");
+		}
+
 		
 	}
 	
+	public void Replay(){		
+		System.out.println("RIGIOCA (R) | ESCI (X)");
+		Scanner sc = new Scanner(System.in);
+	    String i = sc.next();
+	    System.out.println(i);
+	    if (i.toLowerCase().equals("r")) {
+	    	execute();
+	    } else {
+	    	System.exit(0);
+	    }
+	}
 	
-	
-	public boolean moreCards(){
-		System.out.println("CARTA (C) | STO BENE (S)");
+	public boolean moreCards(){		
 		Scanner sc = new Scanner(System.in);
 	    String i = sc.next();
 	    System.out.println(i);
@@ -52,7 +82,27 @@ public class Turn {
 		return valoreCarta;
 	}
 	
-	
+	/*
+	 * * UTILITIES da spostare in altro pacchetto
+	 * 
+	 * 
+	 * 
+	 */
+	public final static void clearConsole() {
+		for (int i = 0; i < 50; ++i) {
+			System.out.println();
+		}			
+//		try {
+//			final String os = System.getProperty("os.name");
+//			if (os.contains("Windows")) {
+//				Runtime.getRuntime().exec("cls");
+//			} else {
+//				Runtime.getRuntime().exec("clear");
+//			}
+//		} catch (final Exception e) {
+//			// Handle any exceptions.
+//		}
+	}
 
 	
 	/*
